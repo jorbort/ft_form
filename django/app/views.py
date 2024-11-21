3599b7a78806ac2ad43d0f15b1249ab705237daf
jorbort <jorbort92@gmail.com>
2024-11-21 19:26:58 +0100

    cambios

--- django/app/views.py
+++ django/app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
 # Create your views here.
+
+def hello_world(request):
+	return render(request,'index.html',{})
