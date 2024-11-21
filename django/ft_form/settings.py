5c87dceadc1e94733cc9a5cb3ad4c4e5a8d71733
Jorge Bortolotti Suarez <jbortolo@car10s3.42barcelona.com>
2024-11-21 19:55:39 +0100

    static folder added

--- django/ft_form/settings.py
+++ django/ft_form/settings.py
@@ -126,21 +126,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-	BASE_DIR / "static"
-]
-
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR/ 'media'
-
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -158,8 +143,16 @@ SECRET = os.environ.get('SECRET')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+	BASE_DIR / "static"
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/ 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
