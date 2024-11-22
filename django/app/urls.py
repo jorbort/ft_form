2c0044c2187b4bd92ae67155dfa1f7fdd9b99e8f
jorbort <jorbort92@gmail.com>
2024-11-22 10:59:06 +0100

    areglados algunos errores de import paths que daban warnings y errores

--- django/app/urls.py
+++ django/app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', hello_world), #http://localhost:8000
-	path('/login', views.login_page, name='login_page')
+	path('', views.hello_world), #http://localhost:8000
+	path('login', views.login_page, name='login_page')
 ]
